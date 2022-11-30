e7250b8dd3c2099fdfb6723f973d542707cb4987
Vizvul <113857968+Vizvul@users.noreply.github.com>
2022-11-30 16:54:35 +0300

    Lesson 13 HT is done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        AutoSpaceNormalased test = new AutoSpaceNormalased();
+        File in = new File("/Users/Stas/Desktop/java/tmp/TestIn.txt");
+        File out = new File("/Users/Stas/Desktop/java/tmp1/TestOut.txt");
+
+        test.normalisedSpace(in,out);
+    }
 }
